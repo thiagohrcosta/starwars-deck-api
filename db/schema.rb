b7e304b701a28187c086a18801977c1cacb30c8f
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-21 18:43:21 -0300

    Add name to cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_213221) do
+ActiveRecord::Schema.define(version: 2021_06_21_214300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_213221) do
     t.string "set"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
