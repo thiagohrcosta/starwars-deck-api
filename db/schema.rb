8784b703d0cda78194e4f787c1848d1ddccbb58e
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-21 18:34:27 -0300

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_212755) do
+ActiveRecord::Schema.define(version: 2021_06_21_213221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "affiliation"
+    t.string "color"
+    t.integer "cost"
+    t.integer "health"
+    t.string "type"
+    t.string "rarity"
+    t.string "set"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
