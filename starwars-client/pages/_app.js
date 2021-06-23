f0cc4d66266b83003f99cbecddd2a4d3afd67850
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-23 11:31:01 -0300

    Add favicon

--- starwars-client/pages/_app.js
+++ starwars-client/pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Star Wars - API</title>
+        <link ref="icon" href="/favicon.icon" />
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
+  
+
 }
 
 export default MyApp
